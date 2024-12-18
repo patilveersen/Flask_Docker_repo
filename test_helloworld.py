182af09ac676ce02453f1c36b9db7ac85b684377
Veer <patil.veersen@gmail.com>
2024-12-18 12:53:42 +0800

    unit test

---
+++ test_helloworld.py
@@ -0,0 +1,13 @@
+import pytest
+from helloworld import app
+
+
+# proxy to a live server.
+@pytest.fixture
+def client():
+    return app.test_client()
+
+
+def test_home(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
